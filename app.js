f2eccfc9a599d7cff33badd49328dac6910e169f
poojakumarasamy <poojakumarasamy10@gmail.com>
2024-12-21 14:14:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import './style.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
